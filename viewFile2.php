59acf400ba77effb61a981f6ac6967c167642235
JosiahBoswell <josiah.s.boswell@gmail.com>
2015-02-18 06:43:25 -0500

    Fixed MySQL-PHP plugin not working.

--- viewFile2.php
+++ viewFile2.php
@@ -5,8 +5,6 @@
  *
  * Takes queries and sends them to the python script.
  */
-ini_set('display_errors',1);
-error_reporting(E_ALL);
 ?>
 <style>
 table,th,td
@@ -25,25 +23,30 @@ error_reporting(E_ALL);
 $conn = new mysqli("localhost","josiah","broncos131","atompy");
 if($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
-}
-
+} else echo 'SUCCESS<br>';
+/*
 //Get our args
 $Z = (string)$_POST["Z"];
 $N = (string)$_POST["N"];
 $SheetNum = (int)$_POST["SheetNum"];
 $BackupArg = (string)$_POST["BackupArg"];
-
+*/
 //Now get our file via our python download bot
 //$result = shell_exec("python WebAPI.py $Z $N $SheetNum $BackupArg 2>&1");
-
-//Print the result
 //echo $result;
 $sql = "show tables";
 $result = $conn->query($sql);
 if($result->num_rows > 0) {
 	while($row = $result->fetch_assoc()) {
-		echo row["Tables_in_atompy"];
+		echo $row['Tables_in_atompy'];
 	}
 }
 
+$sql = "select * from backup";
+$result = $conn->query($sql);
+if($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		print_r($row);
+	}
+}
 ?>
