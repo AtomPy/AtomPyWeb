912eaa265a3857c5197d54a79d8c0a9491dca5e9
JosiahBoswell <josiah.s.boswell@gmail.com>
2015-02-18 07:42:32 -0500

    Reworked the viewFile.php script to pretty much only call the python script to read the excel file. Backup handling will be done on the index page.

--- viewFile.php
+++ viewFile.php
@@ -3,10 +3,9 @@
  *
  * Created by Josiah Lucas Boswell (www.josiahboswell.com)
  *
- * Takes queries and sends them to the python script.
+ * Takes a filename and sheet number as arguements and sends them to the viewFile python script. 
+ * The excel file is then displayed.
  */
-ini_set('display_errors',1);
-error_reporting(E_ALL);
 ?>
 <style>
 table,th,td
@@ -17,20 +16,22 @@ padding:5px;
 }
 </style>
 <a href="index.php">Home</a>
+
 <?php
+//Error handling
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
-//Get our args
-$Z = (string)$_POST["Z"];
-$N = (string)$_POST["N"];
+//Get our args from PHP
+$filename = (string)$_POST["filename"];
 $SheetNum = (int)$_POST["SheetNum"];
-$BackupArg = (string)$_POST["BackupArg"];
 
-//Now get our file via our python download bot
-$result = shell_exec("python WebAPI.py $Z $N $SheetNum $BackupArg 2>&1");
+//Does the file exist in the database?
+if(!file_exists('Database//' . $filename)) {
+	echo "<p><a href='index.php'>Home</a><br>ERROR: File not found: " . $filename . "</p>";
+	exit(1);
+}
 
-//Print the result
-echo $result;
-
+//Call our python script and print out the excel file to the browser
+echo shell_exec("python viewFile.py $filename $SheetNum 2>&1");
 ?>
