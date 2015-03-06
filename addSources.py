960ecea02c8baa3d45c81ca35f4aa778da3fc699
JosiahBoswell <josiah.s.boswell@gmail.com>
2015-03-06 14:29:09 -0500

    File management stuffs.

---
+++ addSources.py
@@ -0,0 +1,7 @@
+########################
+# addSources.py for AtomPy 2.1
+#
+# Created by Josiah 'Lucas' Boswell (www.josiahboswell.com)
+#
+# Opens up the file and adds hyperlinks to the numbers to the appropriate sources.
+########################
