33abd5e10f8b7b302dfcf78c5c1221e2672ae75d
JosiahBoswell <josiah.s.boswell@gmail.com>
2015-03-06 20:04:29 -0500

    Figured out that I will just need to do the source system semi-manually. Removed the automated code for it. Also did some file cleanup and corrected some file naming errors in the database itself.

--- addSources.py
+++
@@ -1,7 +0,0 @@
-########################
-# addSources.py for AtomPy 2.1
-#
-# Created by Josiah 'Lucas' Boswell (www.josiahboswell.com)
-#
-# Opens up the file and adds hyperlinks to the numbers to the appropriate sources.
-########################
