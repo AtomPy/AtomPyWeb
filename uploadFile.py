3402ffc7b3f7290a19e92e95dcb993c3b8caae5d
JosiahBoswell <josiah.s.boswell@gmail.com>
2015-03-30 16:47:44 -0400

    Redid uploadFile script to include more extensive error checking along with a log file. All four steps of the upload process are called upon within this master scripts. Order of operations are Prune, Validate, Backup, and Reconstruct.

--- uploadFile.py
+++ uploadFile.py
@@ -7,12 +7,14 @@
 # currently in the database and compare the two for data validation. Then
 # create a new workbook that copies the original data from the original file,
 # and then goes ahead and adds the new data and hyperlinks.
+#
+# Order of operations: Prune, Validate, Backup, Reconstruct
 ########################
 
 #Includes
 import sys, time, os, shutil
 import openpyxl, xlsxwriter
-import validateFile, backupFile
+import validateFile, backupFile, pruneFile, reconstructFile
 
 #Get the filename of the file we will be working with
 filename = str(sys.argv[1])
@@ -25,6 +27,15 @@ backupDIR = 'Backups//'
 #Open up the log file
 log = open('log.txt','a')
 
+#Prune both the original and uploaded files
+try:
+	pruneFile.pruneFile(databaseDIR + filename)
+	pruneFile.pruneFile(uploadDIR + filename)
+except:
+	log.write('FAILED,' + time.strftime("%H_%M_%S_%m_%d_%Y") + ',' + filename + ', Error occured while pruning.')
+	log.close()
+	sys.exit(1)
+
 #Validate uploaded file against the original one
 result = validateFile.validateFile(databaseDIR + filename, uploadDIR + filename)
 if 'ERROR' in result:
@@ -33,13 +44,23 @@ if 'ERROR' in result:
 	sys.exit(1)
 
 #Backup the original datafile
-backupFile.backupFile(databaseDIR, backupDIR, filename)
+try:
+	backupFile.backupFile(databaseDIR, backupDIR, filename)
+except:
+	log.write('FAILED,' + time.strftime("%H_%M_%S_%m_%d_%Y") + ',' + filename + ', Error occured while backing up file.')
+	log.close()
+	sys.exit(1)
 
 #Reconstruct the database file with the new data
-#reconstructFile.reconstructFile()
+try:
+	reconstructFile.reconstructFile(databaseDIR + backupDIR + filename, uploadDIR + filename, filename)
+except:
+	log.write('FAILED,' + time.strftime("%H_%M_%S_%m_%d_%Y") + ',' + filename + ', Error occured while reconstructing file.')
+	log.close()
+	sys.exit(1)
 
 #Log that everything went alright
-log.write('SUCCESS,' + time.strftime("%H_%M_%S_%m_%d_%Y") + ',' + filename)
+log.write('SUCCESS,' + time.strftime("%H_%M_%S_%m_%d_%Y") + ',' + filename + ', No errors.')
 
 #Close log file
 log.close()
